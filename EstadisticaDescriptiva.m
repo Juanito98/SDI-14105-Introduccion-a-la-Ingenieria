59fd37932ed42b7ef73866029edd8b86cb4f51f4
Juanito98 <juanito.timmy@gmail.com>
2017-10-12 22:23:40 -0500

    Ejercicio
    añadí la campana de gauss

--- EstadisticaDescriptiva.m
+++ EstadisticaDescriptiva.m
@@ -1,3 +1,4 @@
+
 y = [6.495 6.595 6.615 6.635 6.485 6.555 6.665 6.505 6.435 6.625 ...
     6.715 6.655 6.755 6.625 6.715 6.575 6.655 6.605 6.565 6.515 ...
     6.395 6.775 6.685];
@@ -30,3 +31,6 @@ display(yH);
 
 hold on;
 plot(xH, yH);
+
+yGauss = gaussmf(h.BinEdges, [ySigma yPromedio])
+plot(h.BinEdges, yGauss);
