3b58fabf9dd36e3aa230f407c47671f54af6ef7b
Juanito98 <juanito.timmy@gmail.com>
2017-10-16 12:18:18 -0500

    Update rCuadrada.m

--- rCuadrada.m
+++ rCuadrada.m
@@ -2,9 +2,11 @@ function [ r2 ] = rCuadrada( y, yEstimada )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
+yPromedio = mean(y);
 ssr = sum((y - yEstimada).^2);
-sst = sum((y - mean(y)).^2);
-r2 = (1 - ssr) / sst;
-
+sst = sum((y - yPromedio).^2);
+r2 = 1 - ssr/sst;
+sse = sum((yEstimada - yPromedio).^2);
+r2 = sse/sst;
 end
 
