101e1fd66aa003d8065b297d1027332d3866ebf5
Juanito98 <juanito.timmy@gmail.com>
2017-10-16 12:51:27 -0500

    Update regresion.m

--- regresion.m
+++ regresion.m
@@ -1,24 +1,24 @@
-% Regresión Lineal
+% RegresiÃ³n Lineal
 x = 10 : 10 : 80
 y = [25,70,380,550,610,1120,830,1450]
 scatter(x, y);
-
-m = 19;
-b = -200;
-yEstimada = m * x + b
-
-hold on;
-plot(x, yEstimada);
-
-%c = corrcoef(y, yEstimada)
-%R2 = c(1, 1)^2
-R2 = rCuadrada(y, yEstimada)
+yPromedio = mean(y);
 
 p = polyfit(x, y, 1); % Encuentra el polinomio que se ajusta a la minima suma de los cuadrados de la diferencia
 mOpt = p(1);
 bOpt = p(2);
-yOpt = mOpt * x + bOpt;
+yEstimada = mOpt * x + bOpt;
 
-yOpt = polyval(p, x) % Lo mismo que arriba
-plot(x, yOpt);
-R2 = rCuadrada(y, yOpt)
+yEstimada = polyval(p, x) % Lo mismo que arriba
+hold on;
+plot(x, yEstimada);
+c = corrcoef(y, yEstimada)
+R2 = c(1, 2)^2
+
+promedio = yPromedio + zeros(length(x));
+plot(x, promedio);
+
+R2 = rCuadrada(y, yEstimada)
+
+c = edCorrcoef(y, yEstimada)
+R2 = c(1, 2)^2
